4a3500b380bc9d9faea770160581da8afccd980a
alittlebao <walkwithdream@163.com>
2017-02-24 22:47:38 +0800

    first commit

---
+++ src/main/java/com/itest/ITest2.java
@@ -0,0 +1,7 @@
+package com.itest;
+
+public class ITest2 {
+	public static void main(String[] args) {
+		System.out.println("This is test2!");
+	}
+}
---
+++ src/test/java/com/test/ITest.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class ITest {
+	public static void main(String[] args) {
+		System.out.println("This is a test!");
+	}
+}
